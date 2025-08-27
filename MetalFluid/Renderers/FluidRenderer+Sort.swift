afc99e34f4490609f43bc8ddbc09c2cd4f204ec5
tatsuya ogawa <togawa83@gmail.com>
2025-08-27 17:25:30 +0900

    Revert "double buffer"
    This reverts commit e3db184d104a30f40184a88cf202696b2ba15ca4.

--- MetalFluid/Renderers/FluidRenderer+Sort.swift
+++ MetalFluid/Renderers/FluidRenderer+Sort.swift
@@ -176,8 +176,8 @@ extension MPMFluidRenderer{
         commandBuffer.commit()
         commandBuffer.waitUntilCompleted()
         
-        // Swap particle buffers using new method
-        swapParticleBufferWith(&sortedParticleBuffer)
+        // Swap particle buffers
+        swap(&particleBuffer, &sortedParticleBuffer)
     }
     
     internal func bitonicSort(commandBuffer: MTLCommandBuffer) {
@@ -338,7 +338,7 @@ extension MPMFluidRenderer{
             reorderParticles(commandBuffer: commandBuffer)
             
             // Swap particle buffers
-            swapParticleBufferWith(&sortedParticleBuffer)
+            swap(&particleBuffer, &sortedParticleBuffer)
             
         case .radixSort:
             // 1. Extract sort keys for radix sort
@@ -351,7 +351,7 @@ extension MPMFluidRenderer{
             reorderParticlesRadix(commandBuffer: commandBuffer)
             
             // Swap particle buffers
-            swapParticleBufferWith(&radixSortedParticleBuffer)
+            swap(&particleBuffer, &radixSortedParticleBuffer)
         }
         
         commandBuffer.commit()
